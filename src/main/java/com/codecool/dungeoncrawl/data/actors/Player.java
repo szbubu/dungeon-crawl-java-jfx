86937312578b3a8e9d6200670fa2336d5cdf16ac
Nikolett Hevizi <nikihevizi@gmail.com>
2023-09-26 14:38:00 +0200

    Import List + Item to Player

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
@@ -1,8 +1,10 @@
 package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.items.Item;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Player extends Actor {
     private final List<Item> inventory = new LinkedList<Item>();
