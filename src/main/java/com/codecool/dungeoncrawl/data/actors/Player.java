5c9a7d0fd0d23b6f842d4f31918e8c314f1a4302
naaaani <nandi.zalka@gmail.com>
2023-09-26 14:29:01 +0200

    Add inventory field to Player.

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
@@ -2,12 +2,19 @@ package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
 
+import java.util.LinkedList;
+
 public class Player extends Actor {
+    private final List<Item> inventory = new LinkedList<Item>();
     public Player(Cell cell) {
         super(cell);
     }
 
     public String getTileName() {
         return "player";
+    }
+
+    public List<Item> getInventory() {
+        return new LinkedList<Item>(this.inventory);
     }
 }
