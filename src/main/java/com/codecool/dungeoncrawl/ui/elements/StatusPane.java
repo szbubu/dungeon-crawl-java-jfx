fb468ac2525e87a8a4c5a12d67783fff0b4d5f4f
naaaani <nandi.zalka@gmail.com>
2023-09-27 19:24:28 +0200

    Add pretty inventory list.

--- src/main/java/com/codecool/dungeoncrawl/ui/elements/StatusPane.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/elements/StatusPane.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
+import java.util.List;
+
 public class StatusPane {
     public static final int RIGHT_PANEL_WIDTH = 200;
     public static final int RIGHT_PANEL_PADDING = 10;
@@ -14,6 +16,7 @@ public class StatusPane {
 
     private Label inventoryTextLabel;
     private Label inventoryValueLabel;
+    private int inventoryYPosition;
 
     public StatusPane() {
         ui = new GridPane();
@@ -21,6 +24,7 @@ public class StatusPane {
         healthValueLabel = new Label();
         inventoryTextLabel = new Label("Inventory: ");
         inventoryValueLabel = new Label();
+        inventoryYPosition = 5;
     }
 
     public BorderPane build() {
@@ -31,7 +35,6 @@ public class StatusPane {
         ui.add(healthTextLabel, 0, 0);
         ui.add(healthValueLabel, 1, 0);
         ui.add(inventoryTextLabel, 0, 5);
-        ui.add(inventoryValueLabel,1, 5);
 
         BorderPane borderPane = new BorderPane();
         borderPane.setRight(ui);
@@ -42,7 +45,17 @@ public class StatusPane {
         healthValueLabel.setText(text);
     }
 
-    public void setInventoryValue(String text) {
-        inventoryValueLabel.setText(text);
+    public void setInventoryValue(List<String> descriptions) {
+        int numberOfLabels = 0;
+        for (String description : descriptions) {
+            numberOfLabels ++;
+            addItemValueLabel(description, numberOfLabels);
+            inventoryValueLabel.setText(description);
+        }
+    }
+
+    public void addItemValueLabel(String description, int numberOfLabels) {
+        Label itemLabel = new Label(description);
+        ui.add(itemLabel, 0, (inventoryYPosition + numberOfLabels * 5));
     }
 }
